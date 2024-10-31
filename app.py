e2328684f6cce186d7cc6a49a6c582ee61c56c94
Bianca Machado <bianca.machado@germinare.org.br>
2024-10-31 18:51:05 -0300

    Fixing Port

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 import redis, json
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 
@@ -43,6 +44,6 @@ def check_dayhint_exists():
         return jsonify({"message": "A chave 'dicasDoDia' não existe."}), 404
 
     
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
