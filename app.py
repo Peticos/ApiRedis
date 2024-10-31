6051c56131ec9e39b9bb90cc6d9d6a40cabdd815
Bianca Machado <bianca.machado@germinare.org.br>
2024-10-31 19:20:32 -0300

    flasgger dayhint

--- app.py
+++ app.py
@@ -2,14 +2,64 @@ from flask import Flask, jsonify, request
 import redis, json
 from datetime import datetime
 import os
+from flasgger import Swagger
+
 
 app = Flask(__name__)
+swagger = Swagger(app)  # Adicione isso para inicializar o Swagger
+
 
 redis_url = 'rediss://default:AVNS_QnEs5Fi6ZSzEKrXrgLJ@peticos-cache-bhavatechteam-a076.h.aivencloud.com:17964'
 r = redis.Redis.from_url(url=redis_url, ssl_cert_reqs=None)
 
-@app.route('/dayhint', methods=['POST'])
+@app.route('/dayhint/insert', methods=['POST'])
 def set_dayhint():
+    """Adiciona dicas do dia
+    ---
+    tags:
+      - Dicas do Dia
+    parameters:
+      - in: body
+        name: dicas
+        required: true
+        schema:
+          type: array
+          items:
+            type: object
+            properties:
+              hint_text:
+                type: string
+                example: "Ofereça rações específicas para filhotes e evite comida caseira."
+              idHint:
+                type: integer
+                example: 8
+              link:
+                type: string
+                example: "http://link8.com"
+              title:
+                type: string
+                example: "Alimentação adequada para filhotes"
+    responses:
+      200:
+        description: Sucesso
+        schema:
+          type: array
+          items:
+            type: object
+            properties:
+              hint_text:
+                type: string
+                example: "Ofereça rações específicas para filhotes e evite comida caseira."
+              idHint:
+                type: integer
+                example: 8
+              link:
+                type: string
+                example: "http://link8.com"
+              title:
+                type: string
+                example: "Alimentação adequada para filhotes"
+    """
     expiration_time = 24 * 60 * 60
 
     dicas = request.get_json()
@@ -26,6 +76,31 @@ def set_dayhint():
 
 @app.route('/dayhint', methods=['GET'])
 def get_dayhint():
+    """Resgatar dicas do dia
+    ---
+    tags:
+      - Dicas do Dia
+    responses:
+      200:
+        description: Sucesso
+        schema:
+          type: array
+          items:
+            type: object
+            properties:
+              hint_text:
+                type: string
+                example: "Ofereça rações específicas para filhotes e evite comida caseira."
+              idHint:
+                type: integer
+                example: 8
+              link:
+                type: string
+                example: "http://link8.com"
+              title:
+                type: string
+                example: "Alimentação adequada para filhotes"
+    """
     dicas = r.lrange("dicasDoDia", 0, -1)  # Recupera todos os itens da lista
     dicas_decoded = [json.loads(dica) for dica in dicas]  # Desserializa cada item
 
@@ -34,6 +109,28 @@ def get_dayhint():
 
 @app.route('/dayhint/check', methods=['GET'])
 def check_dayhint_exists():
+    """Verifica se a chave 'dicasDoDia' existe no Redis
+    ---
+    tags:
+      - Dicas do Dia
+    responses:
+      200:
+        description: A chave 'dicasDoDia' existe.
+        schema:
+          type: object
+          properties:
+            message:
+              type: string
+              example: "A chave 'dicasDoDia' existe."
+      404:
+        description: A chave 'dicasDoDia' não existe.
+        schema:
+          type: object
+          properties:
+            message:
+              type: string
+              example: "A chave 'dicasDoDia' não existe."
+    """
     # Verifica se a chave "dicasDoDia" existe no Redis
     exists = r.exists("dicasDoDia")
 
